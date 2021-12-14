aeb5b0c6c96229bfa950f5d4aca78069efbe0b04
barc343 <bartosz.cieslak@protonmail.com>
2021-12-14 22:21:52 +0100

    added login and register with requests api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route, Link} from "react-router-dom";
+import {HomeComponent} from "./components/home";
+import {useState} from "react";
+import {HeaderComponent} from "./components/core/header";
+import {AuthComponent} from "./components/core/auth";
+import {FooterComponent} from "./components/core/footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [token, setToken] = useState(sessionStorage.getItem('token') ? sessionStorage.getItem('token') : null)
+    return (
+        <div className="App">
+            {token &&
+                <HeaderComponent/>
+            }
+            <Routes>
+                <Route path="/" element={token ? <HomeComponent/> : <AuthComponent setToken={setToken}/>}/>
+            </Routes>
+            {token &&
+                <FooterComponent/>
+            }
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/core/auth.js
@@ -0,0 +1,113 @@
+import {Button, Card, Container, Form} from "react-bootstrap";
+import {useRef, useState} from "react";
+import {apiHandler} from "../../modules/requests";
+
+const RegisterComponent = ({setRegister}) => {
+    const [login, setLogin] = useState(null)
+    const [email, setEmail] = useState(null)
+    const [pass, setPass] = useState(null)
+
+    const registerUser = (e) => {
+        e.preventDefault()
+        apiHandler.registerUser({'email': email, 'username': login, 'password': pass}).then(resp => {
+            if (resp.hasOwnProperty('email')) {
+                alert('User registered successfully')
+                setRegister(false)
+            } else {
+                alert(resp)
+            }
+            // window.location.reload()
+        })
+    }
+
+    return (
+        <Container className={'mt-5'}>
+            <Card style={{ width: '30rem', 'margin': '0 auto' }}>
+                <Card.Body>
+                    <h2>Register Page <Button onClick={() => setRegister(false)} size={'sm'} variant={'light'}>Login</Button></h2>
+                    <hr/>
+                    <div className={'text-start'}>
+                        <Form onSubmit={(e) => registerUser(e)}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control required onChange={(e) => setLogin(e.target.value)} type="text" placeholder="Enter nickname" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control required onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control required onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </div>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
+}
+
+const LoginComponent = ({setRegister, setToken}) => {
+    const [login, setLogin] = useState(null)
+    const [pass, setPass] = useState(null)
+
+    const loginUser = (e) => {
+        e.preventDefault()
+        apiHandler.loginUser({'username': login, 'password': pass}).then(resp => {
+            if (resp.hasOwnProperty('access')) {
+                sessionStorage.setItem('token', JSON.stringify(resp))
+                setToken(resp)
+            } else alert('Incorrect login data')
+            // window.location.reload()
+        })
+    }
+
+    return (
+        <Container className={'mt-5'}>
+            <Card style={{ width: '30rem', 'margin': '0 auto' }}>
+                <Card.Body>
+                    <h2>Login Page <Button onClick={() => setRegister(true)} size={'sm'} variant={'light'}>Register</Button></h2>
+                    <hr/>
+                    <div className={'text-start'}>
+                        <Form onSubmit={(e) => loginUser(e)}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control onChange={(e) => setLogin(e.target.value)} type="text" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </div>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
+}
+
+export const AuthComponent = ({setToken}) => {
+    const [register, setRegister] = useState(false)
+
+    if (register) {
+        return <RegisterComponent setRegister={setRegister}/>
+    } else {
+        return <LoginComponent setRegister={setRegister} setToken={setToken}/>
+    }
+}
