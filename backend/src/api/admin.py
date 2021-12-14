dbf241af7fcd79030a03b2260a1817baab06956e
barc343 <bartosz.cieslak@protonmail.com>
2021-12-14 19:24:39 +0100

    added core models

--- backend/src/api/admin.py
+++ backend/src/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import IncomeCategory, Income, ExpenseCategory, Expense, BudgetCategory, Budget
 
 # Register your models here.
+
+admin.site.register(IncomeCategory)
+admin.site.register(Income)
+admin.site.register(ExpenseCategory)
+admin.site.register(Expense)
+admin.site.register(BudgetCategory)
+admin.site.register(Budget)
--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.conf import settings
+from datetime import date
+
 
 # Create your models here.
+
+class CoreModel(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class IncomeCategory(CoreModel):
+    pass
+
+
+class Income(CoreModel):
+    category = models.ForeignKey(IncomeCategory, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    date = models.DateField(default=date.today)
+
+
+class ExpenseCategory(CoreModel):
+    pass
+
+
+class Expense(CoreModel):
+    category = models.ForeignKey(ExpenseCategory, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=7, decimal_places=2)
+    date = models.DateField(default=date.today)
+
+
+class BudgetCategory(CoreModel):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+    shared_users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='+',
+        blank=True
+    )
+
+
+class Budget(CoreModel):
+    category = models.ForeignKey(BudgetCategory, on_delete=models.CASCADE)
+    income = models.ManyToManyField(Income, blank=True)
+    expenses = models.ManyToManyField(Expense, blank=True)
